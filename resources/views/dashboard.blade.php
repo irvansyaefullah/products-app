69943b0cef6d6b12d9cb7541733baa665976f33b
irvansyaefullah <irvansyaefullah07@gmail.com>
2024-01-19 23:02:46 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+
+@extends('layouts.app')
+  
+@section('title', 'Dashboard - Laravel Admin Panel With Login and Registration')
+  
+@section('contents')
+  <div class="row">
+    Dashboard
+  </div>
+@endsection
